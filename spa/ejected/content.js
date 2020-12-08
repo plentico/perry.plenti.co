45cc2a0b8f41dda6552c58ab3a2aae5288f00d54
jimafisk <jim.fisk@jantcu.com>
2020-12-08 22:25:56 +0000

    Deploying to gh-pages from  @ 5ab9c12f1806db4b2108329a85b924536934c0f5 🚀

---
+++ spa/ejected/content.js
@@ -0,0 +1,81 @@
+const contentSource = [{
+"path": "/",
+"type": "index",
+"filename": "index.json",
+"fields": {
+	"title": "Customize your planarian!",
+	"options": {
+		"body": [
+			"blue",
+			"orange",
+			"purple",
+			"teal",
+			"yellow"
+		],
+		"eyes": [
+			"brown",
+			"brows",
+			"cat",
+			"blue",
+			"lashes",
+			"teal",
+			"out",
+			"purple",
+			"rick",
+			"tears",
+			"yellow"
+		],
+		"shirts": [
+			"go",
+			"plenti",
+			"rick",
+			"svelte",
+			"yellow"
+		],
+		"accessories": {
+			"antenna": {
+				"width": "100px",
+				"top": "-41px",
+				"left": "151px"
+			},
+			"birthday-hat": {
+				"width": "100px",
+				"top": "-98px",
+				"left": "151px"
+			},
+			"brown-beard": {
+				"width": "145px",
+				"top": "144px",
+				"left": "129px"
+			},
+			"cool-do": {
+				"width": "351px",
+				"top": "-20px",
+				"left": "35px"
+			},
+			"glasses": {
+				"width": "289px",
+				"top": "40px",
+				"left": "62px"
+			},
+			"gray-beard": {
+				"width": "145px",
+				"top": "144px",
+				"left": "129px"
+			},
+			"mask": {
+				"width": "315px",
+				"top": "50px",
+				"left": "49px"
+			},
+			"rick": {
+				"width": "306px",
+				"top": "-62px",
+				"left": "44px"
+			}
+		}
+	}
+}
+},];
+
+export default contentSource;
---
+++ spa/web_modules/svelte/index.js
@@ -0,0 +1,1 @@
+export { SvelteComponentDev as SvelteComponent, afterUpdate, beforeUpdate, createEventDispatcher, getContext, onDestroy, onMount, setContext, tick } from '/spa/web_modules/svelte/internal/index.js';
